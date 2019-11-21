2431c3e446f771367c36b2108545bd29e757275b
Tezan <tezansahu@gmail.com>
2019-11-21 16:43:09 +0530

    code cleanup, cli tool structuring, testing

--- code/textureTransfer.py
+++ code/textureTransfer.py
@@ -3,10 +3,10 @@ from PIL import Image
 from minimumCostPathFunc import minimumCostMask 
 
 
-def Construct(textureImgArray, targetImgArray, blockSize, overlapSize, alpha):
+def Construct(textureImgArray, targetImgArray, blockSize, overlapSize, alpha, tolerance):
     textureImgArray = np.array(textureImgArray)
     targetImgArray = np.array(targetImgArray)
-    print(textureImgArray.shape, targetImgArray.shape)
+    # print(textureImgArray.shape, targetImgArray.shape)
     outSizeX = targetImgArray.shape[0]
     outSizeY = targetImgArray.shape[1]
     [m,n,c] = textureImgArray.shape
@@ -30,7 +30,7 @@ def Construct(textureImgArray, targetImgArray, blockSize, overlapSize, alpha):
             startY = int(j*(blockSize[1] - overlapSize))
             endX = int(min(startX+blockSize[0],outSizeX))
             endY = int(min(startY+blockSize[1],outSizeY))
-            print(startX, endX, startY, endY)
+            # print(startX, endX, startY, endY)
 
             toFill = finalImage[startX:endX,startY:endY,:]
             targetBlock = targetImgArray[startX:endX,startY:endY,:]
@@ -42,11 +42,11 @@ def Construct(textureImgArray, targetImgArray, blockSize, overlapSize, alpha):
                     for y in range(n - targetBlock.shape[1]):
                         blocks1.append(textureImgArray[x:x+targetBlock.shape[0],y:y+targetBlock.shape[1],:]) 
                 blocks1 = np.array(blocks1)
-                matchBlock = MatchBlock(blocks1, toFill, targetBlock, blockSize, alpha)
+                matchBlock = MatchBlock(blocks1, toFill, targetBlock, blockSize, alpha, tolerance)
             # print(toFill.shape, targetBlock.shape)
             #MatchBlock returns the best suited block
             else:
-                matchBlock = MatchBlock(blocks, toFill, targetBlock, blockSize, alpha)
+                matchBlock = MatchBlock(blocks, toFill, targetBlock, blockSize, alpha, tolerance)
             B1EndY = startY+overlapSize-1
             B1StartY = B1EndY-(matchBlock.shape[1])+1
             B1EndX = startX+overlapSize-1
@@ -71,9 +71,14 @@ def Construct(textureImgArray, targetImgArray, blockSize, overlapSize, alpha):
             maskNegate = mask==0
             finalImage[startX:endX,startY:endY,:] = maskNegate*finalImage[startX:endX,startY:endY,:]
             finalImage[startX:endX,startY:endY,:] = matchBlock*mask+finalImage[startX:endX,startY:endY,:]
+
+            completion = 100.0/noOfBlocksInRow*(i + j*1.0/noOfBlocksInCol);
+            print("{0:.2f}% complete...".format(completion), end="\r", flush=True)
+
             if endY == outSizeY:
                 break
         if endX == outSizeX:
+            print("100% complete!", end="\r", flush = True)
             break
     return finalImage
 
@@ -87,30 +92,50 @@ def SSDError(Bi, toFill, targetBlock, alpha):
     lum_target = np.sum(targetBlock, axis = 2)*1.0/3
     lum_toFill = np.sum(toFill, axis = 2)*1.0/3
     error = alpha*np.sqrt(np.sum(((toFill+0.99)>0.1)*(Bi - toFill)*(Bi - toFill))) + (1-alpha)*np.sqrt(np.sum(((lum_toFill+0.99)>0.1)*(lum_Bi - lum_target)*(lum_Bi - lum_target)))
-    return [error,Bi]
+    return [error]
 
-def MatchBlock(blocks, toFill, targetBlock, blockSize, alpha):
-    [minError,bestBlock] = SSDError(blocks[0,:,:,:], toFill, targetBlock, alpha)
+def MatchBlock(blocks, toFill, targetBlock, blockSize, alpha, tolerance):
+    error = []
+    [m,n,p] = toFill.shape
+    bestBlocks = []
+    # count = 0
     for i in range(blocks.shape[0]):
-        [error,Bi] = SSDError(blocks[i,:,:,:], toFill, targetBlock, alpha)
-        if minError > error:
-            bestBlock = Bi
-            minError = error
-    return bestBlock
+        #blocks to be searched are cropped to the size of empty location
+        Bi = blocks[i,:,:,:]
+        Bi = Bi[0:m,0:n,0:p]
+        error.append(SSDError(Bi, toFill, targetBlock, alpha))
+    minVal = np.min(error)
+    bestBlocks = [block[:m, :n, :p] for i, block in enumerate(blocks) if error[i] <= (1.0+tolerance)*minVal]
+    # for i in range(blocks.shape[0]):
+    #     if error[i] <= (1.0+tolerance)*minVal:
+    #         block = blocks[i,:,:,:]
+    #         bestBlocks.append(block[0:m,0:n,0:p])
+    #         count = count+1
+    c = np.random.randint(len(bestBlocks))
+    return bestBlocks[c]
+    
+    
+    # [minError,bestBlock] = SSDError(blocks[0,:,:,:], toFill, targetBlock, alpha)
+    # for i in range(blocks.shape[0]):
+    #     [error,Bi] = SSDError(blocks[i,:,:,:], toFill, targetBlock, alpha)
+    #     if minError > error:
+    #         bestBlock = Bi
+    #         minError = error
+    # return bestBlock
 
-def LoadImage( infilename ) :
-    img = Image.open(infilename).convert('RGB')
-    data = np.asarray(img)
-    return data
+# def LoadImage( infilename ) :
+#     img = Image.open(infilename).convert('RGB')
+#     data = np.asarray(img)
+#     return data
 
-def SaveImage( npdata, outfilename ) :
-    print(npdata.shape)
-    img = Image.fromarray(npdata.astype('uint8')).convert('RGB')
-    img.save( outfilename )
+# def SaveImage( npdata, outfilename ) :
+#     print(npdata.shape)
+#     img = Image.fromarray(npdata.astype('uint8')).convert('RGB')
+#     img.save( outfilename )
 
-texture = np.array(LoadImage('../images/scribble_face.jpg'))
-target = np.array(LoadImage('../images/tendulkar.jpg'))
+# texture = np.array(LoadImage('../images/scribble.jpg'))
+# target = np.array(LoadImage('../images/tendulkar.jpg'))
 
-# print(data.shape)
-out = Construct(texture, target, [40, 40], 10, 0.15)
-SaveImage(out,'output_transfer.png')
+# # print(data.shape)
+# out = Construct(texture, target, [15, 15], 5, 0.1, 0.1)
+# SaveImage(out,'../results/transfer/scribble_tendulkar_b15_o5_a0_1.png')
